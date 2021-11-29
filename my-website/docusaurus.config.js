84b4b86c8265c4ba1ab443e4aada26c037dea779
NZ <naz.goudin@gmail.com>
2021-11-29 20:54:50 +0200

    commit

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://distracted-heisenberg-e8e4de.netlify.app/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
